f90f031725032a690ea742280c22f11111397c86
Will Gleason <wgleason9@gmail.com>
2025-05-31 11:37:53 -0700

    format, test

--- server/google.ts
+++ server/google.ts
@@ -6,6 +6,8 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
   );
 }
 
+console.log(process.env.RAILWAY_PUBLIC_DOMAIN);
+
 export const google = new Google(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
