4d8ab423bf11d1b1759975ec17c773912c5c318d
Will Gleason <wgleason9@gmail.com>
2025-09-01 18:11:58 -0700

    month picker

---
+++ app/dashboard/MonthPicker.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Calendar } from "@/components/ui/calendar";
+import { useRouter } from "next/navigation";
+import * as dateUtils from "@/app/utils/dates";
+
+export function MonthPicker({
+  monthUTC,
+}: {
+  monthUTC: dateUtils.YyyyMm | undefined;
+}) {
+  const router = useRouter();
+  const month =
+    monthUTC ??
+    dateUtils.normYyyyMm(
+      `${new Date().getUTCFullYear()}-${String(new Date().getUTCMonth() + 1)}`
+    );
+  const { end } = dateUtils.monthRangeUTC(month);
+  console.log(end);
+
+  return (
+    <Calendar
+      mode="single"
+      month={end}
+      onMonthChange={(month) => {
+        router.push(`/dashboard?monthUTC=${month.toISOString().slice(0, 7)}`);
+      }}
+      captionLayout="dropdown" // shows month/year dropdown
+      startMonth={new Date(2000, 0)}
+      endMonth={new Date(2030, 0)}
+      showOutsideDays={false}
+      fixedWeeks={false}
+      // hide actual grid
+      styles={{
+        day: { display: "none" },
+        week: { display: "none" },
+        head: { display: "none" },
+        month_grid: { display: "none" },
+      }}
+    />
+  );
+}
